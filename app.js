487228a1dab0ca5567535df606ce4846fa3535cf
Hack Reactor Student <student@hackreactor.com>
2019-07-29 16:17:29 -0700

    Implemented Code

--- app.js
+++ app.js
@@ -1,1 +1,26 @@
 // TODO
+var groceryItems = ['Cucumber', 'Prosciutto', 'Hummus'];
+var App = () => (
+  <div>
+    <h2>My Grocery List</h2>
+    <GroceryList items={groceryItems}/>
+  </div>
+);
+
+var GroceryList = (props) => (
+  <ul>
+    {props.items.map(item => (
+      <GroceryListItem name={item}/>
+    ))}
+  </ul>
+);
+
+var GroceryListItem = (props) => (
+  <li> {props.name} </li>
+);
+
+
+
+
+
+ReactDOM.render(<App />, document.getElementById("app"));
