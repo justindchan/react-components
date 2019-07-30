645740a88d19dce5f6dfe65f225a2b15943401bf
Hack Reactor Student <student@hackreactor.com>
2019-07-29 17:09:49 -0700

    React Solo complete

--- app.js
+++ app.js
@@ -9,16 +9,33 @@ var App = () => (
 
 var GroceryList = (props) => (
   <ul>
-    {props.items.map(item => (
+    {props.items.map(item =>
       <GroceryListItem name={item}/>
-    ))}
+    )}
   </ul>
 );
 
-var GroceryListItem = (props) => (
-  <li> {props.name} </li>
-);
-
+class GroceryListItem extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      done: false
+    }
+  }
+  onListItemClick() {
+    this.setState({
+      done: !this.state.done
+    });
+  }
+  render() {
+    var style = {
+      textDecoration: this.state.done ? 'line-through' : 'none'
+    };
+    return (
+      <li style={style} onClick={this.onListItemClick.bind(this)}>{this.props.name}</li>
+    );
+  }
+}
 
 
 
